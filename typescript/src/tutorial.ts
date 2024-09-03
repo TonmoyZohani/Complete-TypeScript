a5e167575227a6ae455f76fadc89c67372f94ff1
Khaled Zohani Tonmoy <94390552+TonmoyZohani@users.noreply.github.com>
2024-09-03 18:37:45 +0600

    update code

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -514,25 +514,25 @@
 
 // console.log(getColorName(Color.Red));
 
-// type ValueType = string | number | boolean;
+type ValueType = string | number | boolean;
 
-// let value: ValueType;
-// const random = Math.random();
+let value: ValueType;
+const random = Math.random();
 
-// value = random < 0.33 ? "Hello" : random < 0.66 ? 123.653 : true;
+value = random < 0.33 ? "Hello" : random < 0.66 ? 123.653 : true;
 
-// function checkValue(value: ValueType) {
-//   if (typeof value === "string") {
-//     console.log(value.toLowerCase());
-//     return;
-//   }
+function checkValue(value: ValueType) {
+  if (typeof value === "string") {
+    console.log(value.toLowerCase());
+    return;
+  }
 
-//   if (typeof value === "number") {
-//     console.log(value.toFixed(2));
-//     return;
-//   }
+  if (typeof value === "number") {
+    console.log(value.toFixed(2));
+    return;
+  }
 
-//   console.log(`boolean:${value}`);
-// }
+  console.log(`boolean:${value}`);
+}
 
-// checkValue(value);
+checkValue(value);
