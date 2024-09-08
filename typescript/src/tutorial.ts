520d5ddf7b3786a6a4994a5715a8644ec1562412
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-08 22:25:10 +0600

    extends extra

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -679,39 +679,51 @@
 //   console.log(tour.name);
 // });
 
-const url = "https://www.scourse-api.com/react-tours-project";
+// const url = "https://www.scourse-api.com/react-tours-project";
 
-type Tour = {
-  id: string,
-  name: string,
-  info: string,
-  image: string,
-  price: string
-}
+// type Tour = {
+//   id: string,
+//   name: string,
+//   info: string,
+//   image: string,
+//   price: string
+// }
 
-async function fetchData(url: string): Promise<Tour[]> {
-  try {
-    const response = await fetch(url);
+// async function fetchData(url: string): Promise<Tour[]> {
+//   try {
+//     const response = await fetch(url);
 
-    // Check if the request was successful.
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+//     // Check if the request was successful.
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
 
-    const data: Tour[] = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    const errMsg =
-      error instanceof Error ? error.message : "there was an error...";
-    console.error(errMsg);
+//     const data: Tour[] = await response.json();
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     const errMsg =
+//       error instanceof Error ? error.message : "there was an error...";
+//     console.error(errMsg);
 
-    // throw error;
-    return [];
-  }
-}
+//     // throw error;
+//     return [];
+//   }
+// }
 
-const tours = await fetchData(url);
-tours.map((tour) => {
-  console.log(tour.name);
-});
+// const tours = await fetchData(url);
+// tours.map((tour) => {
+//   console.log(tour.name);
+// });
+
+// class Book {
+//   title:string;
+//   author: string;
+
+//   constructor(title:string,author:string){
+//     this.title = title;
+//     this.author = author
+//   }
+// }
+
+// const deepWork = new Book("deep work","cal newport");
