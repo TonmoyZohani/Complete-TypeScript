5503b8646ea61aad3c6d1ef9924aac22d2d18141
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-10 22:20:23 +0600

    type decalerd

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -760,6 +760,7 @@
 class Book {
   private checkedOut: boolean = false;
   constructor(public readonly title: string, public author: string) {}
+
   get info() {
     return `${this.title} by ${this.author}`;
   }
@@ -775,3 +776,9 @@ class Book {
     return `${this.title} by ${this.author}`;
   }
 }
+
+const deepWork = new Book("deep work", "cal newport");
+console.log(deepWork.info);
+// deepWork.checkOut = true;
+console.log(deepWork.someInfo);
+console.log(deepWork.checkOut);
