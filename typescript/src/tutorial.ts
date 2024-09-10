ddf76429096fcc84e8ff7cc235789ed89f713c4b
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-10 22:11:30 +0600

    type decalerd

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -758,20 +758,20 @@
 // console.log(deepWork.isCheckedOut()); // true
 
 class Book {
-  private checkedOut: boolean = false;
-  constructor(public readonly title: string, public author: string) {}
-  get info() {
-    return `${this.title} by ${this.author}`;
-  }
+  // private checkedOut: boolean = false;
+  // constructor(public readonly title: string, public author: string) {}
+  // get info() {
+  //   return `${this.title} by ${this.author}`;
+  // }
 
-  private set checkOut(checkedOut: boolean) {
-    this.checkedOut = checkedOut;
-  }
-  get checkOut() {
-    return this.checkedOut;
-  }
-  public get someInfo() {
-    this.checkOut = true;
-    return `${this.title} by ${this.author}`;
-  }
+  // private set checkOut(checkedOut: boolean) {
+  //   this.checkedOut = checkedOut;
+  // }
+  // get checkOut() {
+  //   return this.checkedOut;
+  // }
+  // public get someInfo() {
+  //   this.checkOut = true;
+  //   return `${this.title} by ${this.author}`;
+  // }
 }
