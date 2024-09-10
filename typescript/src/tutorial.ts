30a7c6d544b63964833a9d49385850031b10b216
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-10 22:20:50 +0600

    type decalerd

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -780,5 +780,5 @@ class Book {
 const deepWork = new Book("deep work", "cal newport");
 console.log(deepWork.info);
 // // deepWork.checkOut = true;
-// console.log(deepWork.someInfo);
-// console.log(deepWork.checkOut);
+console.log(deepWork.someInfo);
+console.log(deepWork.checkOut);
