bcd0e52e09d6ebbcbf3ab076f1d791e6f41b9319
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-13 20:33:35 +0600

    non null assertior operator

--- typescript/src/tasks.ts
+++ typescript/src/tasks.ts
@@ -1,2 +1,14 @@
-const btn = document.querySelector<HTMLButtonElement>(".test-btn")!;
-btn.disabled = true;
+// const btn = document.querySelector<HTMLButtonElement>(".test-btn")!;
+// btn.disabled = true;
+
+const taskForm = document.querySelector<HTMLFormElement>(".form");
+const formData = document.querySelector<HTMLInputElement>(".form-input");
+
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+type Task = {
+  description: string;
+  isCompleted: boolean;
+};
+
+const tasks: Task[] = [];
