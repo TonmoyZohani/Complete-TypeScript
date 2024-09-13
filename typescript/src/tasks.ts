a76a55e0143f2959ef88149e500abf6aa1492da8
TonmoyZohani <tonmoyzohani@gmail.com>
2024-09-13 21:57:14 +0600

    render task

--- typescript/src/tasks.ts
+++ typescript/src/tasks.ts
@@ -27,7 +27,7 @@ function createTask(event: SubmitEvent) {
 
     addTask(task);
 
-    renderTask(task)
+    renderTask(task);
 
     formInput.value = "";
     return;
@@ -41,8 +41,23 @@ function addTask(task: Task): void {
   console.log(tasks);
 }
 
-function renderTask(task:Task):void{
-    const taskElement = document.createElement("li");
-    taskElement.textContent = task.description;
-    taskListElement?.appendChild(taskElement)
+function updateStorage(): void {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTask(task: Task): void {
+  const taskElement = document.createElement("li");
+  taskElement.textContent = task.description;
+  // checkbox
+  const taskCheckbox = document.createElement("input");
+  taskCheckbox.type = "checkbox";
+  taskCheckbox.checked = task.isCompleted;
+  // toggle checkbox
+  taskCheckbox.addEventListener("change", () => {
+    task.isCompleted = !task.isCompleted;
+    updateStorage();
+  });
+
+  taskElement.appendChild(taskCheckbox);
+  taskListElement?.appendChild(taskElement);
 }
