b1e0a231072b6aa9a7cad88217cc2b887f1dd41b
TonmoyZohani <tonmoyzohani@gmail.com>
2025-06-07 12:06:19 +0600

    learned union type

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,23 +1,37 @@
-// interface someValue {
-//   name: string;
-//   id: number;
+/************Type Annotations************ */
+
+// let greeting:string = "This is just a game";
+// console.log(greeting.toUpperCase());
+// let age:number = 50;
+// console.log(age-20);
+// let isAdult:boolean = age>=18;
+// console.log(isAdult)
+
+/************Union Type****************** */
+
+// let tax: number | string;
+// tax = 10;
+// console.log(tax)
+
+// const books = ["Rich Dad & Poor Dad", "1984", "My Name Is Khan"];
+// let foundBook: string | undefined ;
+
+// for (let book of books) {
+//   if (book === "1983") {
+//     foundBook = book;
+//     break;
+//   }
 // }
 
-// let someObj: someValue = {
-//   name: "Tonmoy Zohani",
-//   id: 12345,
-// };
+// console.log(foundBook?.length);
 
-// console.log("Print", someObj);
+// let status: "success" | "pending" | "error";
 
-let greeting:string = "This is just a game";
+// status = "success";
+// console.log(status);
 
-console.log(greeting.toUpperCase());
+/***************Array****************** */
 
-let age:number = 50;
+let randomArr: (number | string)[] = ["Tonmoy", 12];
 
-console.log(age-20);
-
-let isAdult:boolean = age>=18;
-
-console.log(isAdult)
+console.log("Print", randomArr);
